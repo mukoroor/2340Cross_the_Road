5236f2ebb0f9afff4a6aaf6b222b78c44da5ddbe
Mukoro, Oruaroghene Oluwatimileyin <omukoro3@gatech.edu>
2023-02-13 21:22:27 -0500

    Adding sprites, Updated UI, and did checkstyle

--- team18/app/src/main/java/com/example/team18/SpriteSelector.java
+++ team18/app/src/main/java/com/example/team18/SpriteSelector.java
@@ -1,22 +1,23 @@
 package com.example.team18;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.ViewGroup;
+import android.util.TypedValue;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
 public class SpriteSelector extends AppCompatActivity {
-    private static Sprite selected;
     private int pointer = 0;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,12 +41,20 @@ public class SpriteSelector extends AppCompatActivity {
         final int viewWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
         final int viewHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
 
+        /*distributing heights of Layout
+          characterGallery: 50%;
+          spriteDescription: 20%;
+          navigation: 30%;
+        * */
         int percentHeight = (viewHeight / 10);
 
         characterGallery.requestLayout();
+        spriteDescription.requestLayout();
         navigation.requestLayout();
-        characterGallery.setLayoutParams(new LinearLayout.LayoutParams(viewWidth, viewHeight - 4 * percentHeight));
-        navigation.setLayoutParams(new LinearLayout.LayoutParams(viewWidth, 4 * percentHeight));
+        characterGallery.setLayoutParams(new LinearLayout.LayoutParams(viewWidth,
+                viewHeight - 5 * percentHeight));
+        spriteDescription.setTextSize(TypedValue.COMPLEX_UNIT_PX, percentHeight);
+        navigation.setLayoutParams(new LinearLayout.LayoutParams(viewWidth, 3 * percentHeight));
 
 
 
@@ -53,22 +62,29 @@ public class SpriteSelector extends AppCompatActivity {
             pointer = (pointer - 1 + spriteOptions.length) % spriteOptions.length;
             spriteView.setImageResource(spriteOptions[pointer][0]);
             spriteDescription.setText(spriteDescriptions[pointer]);
+            leftButton.setForeground(ContextCompat.getDrawable(this, R.drawable.leftarrowhover));
+            rightButton.setForeground(ContextCompat.getDrawable(this, R.drawable.rightarrow));
         });
 
         rightButton.setOnClickListener(e -> {
             pointer = (pointer + 1 + spriteOptions.length) % spriteOptions.length;
             spriteView.setImageResource(spriteOptions[pointer][0]);
             spriteDescription.setText(spriteDescriptions[pointer]);
+            rightButton.setForeground(ContextCompat.getDrawable(this, R.drawable.rightarrowhover));
+            leftButton.setForeground(ContextCompat.getDrawable(this, R.drawable.leftarrow));
         });
 
-        submitButton.setOnClickListener( e -> {
+
+        submitButton.setOnClickListener(e -> {
             Intent login = new Intent(this, LoginActivity.class);
             login.putExtra("index", pointer);
             startActivity(login);
         });
 
-        returnButton.setOnClickListener( e -> {
-            finish();
+        returnButton.setOnClickListener(e -> {
+            Intent start = new Intent(this, MainActivity.class);
+            start.putExtra("index", pointer);
+            startActivity(start);
         });
     }
 }
