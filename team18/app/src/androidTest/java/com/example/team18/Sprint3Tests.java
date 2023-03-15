b51c8412e6701111c6476c362066d73795f15ab5
Mukoro, Oruaroghene Oluwatimileyin <omukoro3@gatech.edu>
2023-03-14 23:05:07 -0400

    Junits

---
+++ team18/app/src/androidTest/java/com/example/team18/Sprint3Tests.java
@@ -0,0 +1,12 @@
+package com.example.team18;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Sprint3Tests {
+    @Test
+    public void runnIng() {
+        assertEquals(0, 0);
+    }
+}
