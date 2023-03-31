b8eb8ca3d5ae80136c42830223e24bbc5c0ddde1
jayiih3 <jayiih3@gatech.edu>
2023-03-31 13:21:14 -0400

    New junits

---
+++ team18/app/src/androidTest/java/com/example/team18/Sprint4JUnits.java
@@ -0,0 +1,14 @@
+package com.example.team18;
+import org.junit.Test;
+
+public class Sprint4JUnits {
+    @Test
+    public void gameRestart() {
+
+    }
+
+    @Test
+    public void goalHighScore() {
+        
+    }
+}
