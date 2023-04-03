c69d1a033c918a2b90f291c8956185c04ad23c22
jayiih3 <jayiih3@gatech.edu>
2023-04-02 22:28:44 -0400

    Reloaded test cases

--- team18/app/src/androidTest/java/com/example/team18/Sprint4JUnits.java
+++ team18/app/src/androidTest/java/com/example/team18/Sprint4JUnits.java
@@ -1,84 +1,70 @@
 package com.example.team18;
 
-import static org.junit.Assert.assertArrayEquals;
+import static android.content.Intent.getIntent;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+import android.app.Activity;
+import android.app.Instrumentation;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.core.app.ApplicationProvider;
-import androidx.test.espresso.Espresso;
-import androidx.test.espresso.ViewAction;
-import androidx.test.espresso.action.ViewActions;
-import androidx.test.espresso.assertion.ViewAssertions;
-import androidx.test.espresso.matcher.ViewMatchers;
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
-import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 
 import java.util.Random;
-import java.util.concurrent.atomic.AtomicInteger;
 
-public class Sprint4JUnits {
+public class Sprint4Junits {
     Random r = new Random();
 
-    @Rule
-    public ActivityScenarioRule<GameOverScreenActivity> activityScenarioRule2 = new ActivityScenarioRule<>(GameOverScreenActivity.class);
-
     @Test
-    public void gameRestart() {
+    public void savesHighestScore() {
+        Intent playIntent = new Intent(ApplicationProvider.getApplicationContext(),
+            GameOverScreenActivity.class);
+        playIntent.putExtra("finalScore",5);
 
-        Espresso.onView(ViewMatchers.withId(R.id.restart)).perform(ViewActions.click());
-        Espresso.onView(ViewMatchers.withId(R.id.spriteView)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+        // Launch the activity with the intent
+        ActivityScenario<GameOverScreenActivity> scenario = ActivityScenario.launch(playIntent);
 
-    }
-    public int findBlockType(GameBlockTypes g, Game curr) {
-        GameBlock[][] gameBlockArr = curr.getGameBlockArray();
-
-        int startingY = 13;
-
-        while (gameBlockArr[startingY][4].blockType != g) {
-            startingY--;
-        }
-        return startingY;
+        // Assert that the activity is in the resumed state
+        scenario.onActivity(activity -> assertEquals(5, activity.getFinalScore()));
     }
 
-
     @Test
-    public void goalTitleHighScore() {
-        Intent playIntent = new Intent(ApplicationProvider.getApplicationContext(), GameScreenActivity.class);
-        playIntent.putExtra("lives", 5);
-        Sprite player = new Sprite(r.nextInt(4), "TEST");
+    public void gameOverOnNoLives() {
+        Intent playIntent = new Intent(ApplicationProvider.getApplicationContext(),
+            GameScreenActivity.class);
+        playIntent.putExtra("lives",1);
+        Sprite player = new Sprite(3, "TEST");
         playIntent.putExtra("player", player.toString());
-
 
         // Launch the activity with the intent
         ActivityScenario<GameScreenActivity> scenario = ActivityScenario.launch(playIntent);
+
+        // Assert that the activity is in the resumed state
         scenario.onActivity(activity -> {
-            GameScreenActivity g = (GameScreenActivity) activity;
-            Game curr = g.getGame();
-            GameBlockTypes goal = GameBlockTypes.GOAL;
+            Game currGame = activity.getGame();
+            Sprite currGamePlayer = currGame.getPlayer();
+            currGamePlayer.setLives(0);
+            Instrumentation.ActivityMonitor monitor
+                = getInstrumentation().addMonitor((IntentFilter) null, null, false);
+            Activity nextActivity
+                = getInstrumentation().waitForMonitorWithTimeout(monitor, 500);
 
-            int row = findBlockType(goal, curr);
-            int preSocre = 0;
-
-            for (int i = 0; i < 3; i++) {
-                g.moveUp();
-                preSocre = curr.getScore();
-
+            if (nextActivity != null) {
+                Intent t = nextActivity.getIntent();
+                int latestScore = t.getIntExtra("score", -1);
+                assertEquals(0, latestScore);
+                assertEquals(GameOverScreenActivity.class, nextActivity.getClass());
             }
-
-
-            curr.changePosition(0,-9);
-            assertEquals((preSocre ), curr.getScore());
-
         });
+
+
     }
-
-
 }
