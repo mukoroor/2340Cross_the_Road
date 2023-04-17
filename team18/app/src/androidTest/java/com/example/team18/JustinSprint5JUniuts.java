b46fc3483b7648f375c4c557e1b070fcc2168a0c
jayiih3 <jayiih3@gatech.edu>
2023-04-16 20:42:08 -0400

    I added one of my sprint 5 JUnits.

--- team18/app/src/androidTest/java/com/example/team18/JustinSprint5JUniuts.java
+++ team18/app/src/androidTest/java/com/example/team18/JustinSprint5JUniuts.java
@@ -43,53 +43,5 @@ public class JustinSprint5JUniuts {
         });
 
     }
-    @Test
-    public void checkMinecartAttributes(){
-        Intent playIntent = new Intent(ApplicationProvider.getApplicationContext(),
-                GameScreenActivity.class);
-        playIntent.putExtra("Justin",5);
 
-        // Launch the activity with the intent
-        ActivityScenario<GameScreenActivity> scenario = ActivityScenario.launch(playIntent);
-
-        // Assert that the activity is in the resumed state
-        scenario.onActivity(activity -> {
-            GameScreenActivity g = (GameScreenActivity) activity;
-
-            ArrayList<Vehicle> vehicles = g.getVehicleList();
-            boolean found = false;
-
-            // Create a new LinearLayout object with specific properties
-            LinearLayout row = new LinearLayout(g.getApplicationContext());
-            row.setOrientation(LinearLayout.VERTICAL);
-            LinearLayout.LayoutParams rowParams = new LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT,
-                    LinearLayout.LayoutParams.WRAP_CONTENT
-            );
-            row.setLayoutParams(rowParams);
-
-            // Create a new ImageView object with a specific image resource
-            ImageView image = new ImageView(g.getApplicationContext());
-            image.setImageResource(R.drawable.minecarts);
-
-            // Create a new ImageView object with a specific image resource
-            ImageView tracks = new ImageView(g.getApplicationContext());
-            tracks.setImageResource(R.drawable.traintracks);
-
-
-            // Create a new Minecart object
-            Minecart minecart = new Minecart(row, image, tracks);
-
-
-
-            // Check if the parameters match the parameters passed to the constructor
-            assertEquals(row, minecart.getRow());
-            assertEquals(image, minecart.getImage());
-            assertEquals(tracks, minecart.getTracks());
-
-            
-        });
-
-
-    }
 }
