ceccb60bd08ed6b91e846a6006cecc5c30257ae5
Collier, Kelley IV <kcollier32@gatech.edu>
2023-04-16 21:26:42 -0400

    Consolidation

--- team18/app/src/androidTest/java/com/example/team18/Sprint5JUnits.java
+++ team18/app/src/androidTest/java/com/example/team18/Sprint5JUnits.java
@@ -12,14 +12,43 @@ import androidx.test.core.app.ApplicationProvider;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 public class Sprint5JUnits {
     Random r = new Random();
     @Test
+    public void logsHaveDifferentSizes() {
+        Game g = new Game(new Sprite(1,"TEST"),r.nextInt(3) * 2 + 1);
+        g.createGrid();
+        g.populateGrid();
+        ArrayList<Integer> result = logLengthsFinder(Game.getGameBlockArray());
+        Set<Integer> unique = new HashSet<>(result);
+        assertTrue(unique.size() > 1);
+    }
+
+    private ArrayList<Integer> logLengthsFinder(GameBlock[][] arr) {
+        ArrayList<Integer> logLengths = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i][0].blockType == GameBlockTypes.RIVER ||
+                    arr[i][0].blockType == GameBlockTypes.LOG) {
+                int count = 0;
+                for (int k = 0; k < arr[i].length; k++) {
+                    if (arr[i][k].blockType == GameBlockTypes.LOG) {
+                        count++;
+                    }
+                }
+                logLengths.add(count);
+            }
+        }
+        return logLengths;
+    }
+
+    @Test
     public void gwsRestartButton() {
         Intent playIntent = new Intent(ApplicationProvider.getApplicationContext(),
-            GameWinScreenActivity.class);
+                GameWinScreenActivity.class);
         playIntent.putExtra("finalScore",5);
 
         // Launch the activity with the intent
@@ -35,7 +64,7 @@ public class Sprint5JUnits {
     @Test
     public void gwsQuitButton() {
         Intent playIntent = new Intent(ApplicationProvider.getApplicationContext(),
-            GameWinScreenActivity.class);
+                GameWinScreenActivity.class);
         playIntent.putExtra("finalScore",5);
 
         // Launch the activity with the intent
